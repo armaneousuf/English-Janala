770ce5a2df01a8638a01636c33643156ab995b28
armaneousuf <armaneousuf97@gmail.com>
2026-02-16 19:15:26 +0600

    feat: added logics to the words

--- script.js
+++ script.js
@@ -7,53 +7,71 @@ const loadLessons = () => {
 };
 
 const loadLevelWord = (id) => {
-    const url = `https://openapi.programming-hero.com/api/level/${id}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayWords(data.data))
-        .catch(error => console.log('ERROR', error));
-}
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayWords(data.data))
+    .catch((error) => console.log("ERROR", error));
+};
+
+const speakWord = (text) => {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = "en-GB"; // Set to English
+  window.speechSynthesis.speak(utterance);
+};
 
 const displayWords = (words) => {
-    const wordsContainer = document.querySelector("#words-container");
-    wordsContainer.innerHTML = '';
+  const wordsContainer = document.querySelector("#words-container");
+  wordsContainer.innerHTML = "";
 
-    words.forEach((word) => {
+  if (words.length === 0) {
+    wordsContainer.innerHTML = `
+        <div class="text-center col-span-full space-y-5 font-siliguri flex flex-col items-center justify-center">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><title xmlns="">alert-line</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M12 16h.008M12 10v3m-1.425-7.783L3.517 17a1.667 1.667 0 0 0 1.425 2.5h14.116a1.666 1.666 0 0 0 1.425-2.5L13.426 5.217a1.666 1.666 0 0 0-2.85 0"/></svg>
+                </span>
+                <p class="text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h2 class="text-2xl font-semibold">নেক্সট Lesson এ যান</h2>
+               </div>
+        `;
+    return;
+  }
 
-        const divE = document.createElement("div");
-        divE.innerHTML = ` <div class="bg-white p-10 text-center rounded">
-                    <h2 class="text-2xl font-bold">${word.word}</h2>
+  words.forEach((word) => {
+    const divE = document.createElement("div");
+    divE.innerHTML = ` <div class="bg-white p-10 text-center rounded">
+                    <h2 class="text-2xl font-bold">${word.word ? word.word : 'শব্দ পাওয়া যায়নি'}</h2>
                     <p class="text-gray-400 my-5 font-siliguri">${word.pronunciation}</p>
-                    <p class="text-xl font-bold text-indigo-600 font-siliguri">${word.meaning}</p>
+                    <p class="text-xl font-bold text-indigo-600 font-siliguri">${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'}</p>
 
                     <div class="icons flex justify-between items-center mt-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                        <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="size-6 hover:text-indigo-400 cursor-pointer">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
-                        </svg>
+                        </svg></button>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                        <button onclick="speakWord('${word.word}')"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="size-6 hover:text-indigo-400 cursor-pointer">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-                        </svg>
+                        </svg></button>
 
                     </div>
                 </div>`;
-        wordsContainer.append(divE);
-    })
-}
+    wordsContainer.append(divE);
+  });
+};
 
 const displayLessons = (lessons) => {
   // Get the parent
   const lessonContainer = document.querySelector("#lesson-container");
-    lessonContainer.innerHTML = "";
-    
-    for (let lesson of lessons) {
-      // Create the element
-      const lessonDiv = document.createElement("div");
-      lessonDiv.innerHTML = `<button onClick="loadLevelWord('${lesson.level_no}')" class="btn btn-outline btn-sm btn-primary">
+  lessonContainer.innerHTML = "";
+
+  for (let lesson of lessons) {
+    // Create the element
+    const lessonDiv = document.createElement("div");
+    lessonDiv.innerHTML = `<button onClick="loadLevelWord('${lesson.level_no}')" class="btn btn-outline btn-sm btn-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -62,10 +80,9 @@ const displayLessons = (lessons) => {
                         Lesson - ${lesson.level_no}
                     </button>`;
 
-      // Append
-      lessonContainer.append(lessonDiv);
-    }
-
+    // Append
+    lessonContainer.append(lessonDiv);
+  }
 };
 
 loadLessons();
